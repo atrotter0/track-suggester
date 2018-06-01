6e44186361748cfb5c14ec216b31f44e11cc4a9e
Abel Trotter <a_trotter0@hotmail.com>
2018-06-01 09:40:26 -0700

    Check for localStorage before building objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
-  console.log('loaded!');
+// number of question sets
+const SET_ONE = 1;
+const SET_TWO = 2;
+const SET_THREE = 3;
+const SET_FOUR = 4;
+const SET_FIVE = 5;
 
-  $("#next").click(function(e) {
+// check for local storage files first, then create or next
+function checkStorage() {
+  var count = localStorage.getItem("count");
+  if (count === null) createSurveyObjects();
+}
+
+function createSurveyObjects() {
+  console.log("creating survey...");
+}
+
+$(document).ready(function() {
+  checkStorage();
+
+  $("#formBtn").click(function(e) {
     e.preventDefault();
   });
 });
